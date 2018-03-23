6006336c21efb30adc6f249a3b03367bb90b74d9
Sydney Barlow <sydneybarlow44@gmail.com>
2018-03-23 00:03:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-{
-  function happyHolidays() {
+function happyHolidays() {
     return "Happy Holidays"
   }
-}
-{
-  function happyHolidaysTo(name) {
+function happyHolidaysTo(name) {
     return `Happy holidays, ${name}!`;
   }
-}
-{
-  function happyHolidayTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
     return `Happy ${holiday}, ${name}!`;
   }
-}
-{
-  function holidayCountdown(days, holiday) {
+function holidayCountdown(days, holiday) {
     return `It's ${days} days until ${holiday}!`;
   }
-}
